54e9a124dba1f16ecb7753f303c3816076f99abd
Gökhan SÖNMEZ <gsonmez828@aof.anadolu.edu.tr>
2021-03-10 23:17:35 +0300

    v2

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category';
 
 @Component({
   selector: 'app-category',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class CategoryComponent implements OnInit {
 
   constructor() { }
-   categories:any = ""
+   categories:Category[] = []
   ngOnInit(): void {
   }
 
